b8c54cb16d3866b0b84c659189e7642032556d1e
csperlich <csperlich@gmail.com>
2013-11-27 15:27:12 -0500

    minor updates

--- urlinfo.h
+++ urlinfo.h
@@ -10,16 +10,16 @@
 #define HOST_LENGTH 128
 #define PATH_LENGTH 2048
 
-struct urlinfo
+typdef struct
 {
 	char *host;
 	char *path;
 	struct urlinfo *next;
-};
+} urlinfo;
 
 /*
  * construct a url from a (possibly) relative address
  */
-struct urlinfo makeURL(char *givenAddress, char *currentURL);
+urlinfo makeURL(char *givenAddress, char *currentURL);
 
 #endif
