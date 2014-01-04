c92ecdf27d6a222114da4cef842a04bcae3f26ac
csperlich <csperlich@gmail.com>
2014-01-04 17:16:31 -0500

    add clean_search_results(), removes jargon links from search request

--- utils/parser.c
+++ utils/parser.c
@@ -21,10 +21,10 @@ parser *init_parser(char *regex)
 		printf("error comiling regen");
 		return NULL;
 	}
-
+    
 	// study regexp so pcre_exec can be optimized
 	p->study = pcre_study(p->re, 0, &error);
-
+    
 	/*
 	 * set the vector to hold results
 	 * each pair in the first 2/3 of vectorsize holds the beginning
@@ -46,13 +46,13 @@ void parse_all(parser *p, char *text, size_t textlen, string_llist *destination)
 	while (1)
 	{
 		// find matches for re with text, optimized w/ study
-		int retval = pcre_exec(p->re, NULL,//p->study, 
-			text, textlen, offset, 0, 
-			vector, p->vectorsize);
+		int retval = pcre_exec(p->re, NULL,//p->study,
+                               text, textlen, offset, 0,
+                               vector, p->vectorsize);
 		// if no matches found, break
 		if (retval < 0)
 			break;
-
+        
 		// get start and one-past-the-end of the first part of the regex
 		int stringbegin = vector[0];
 		int stringend = vector[1];
@@ -67,7 +67,7 @@ void parse_all(parser *p, char *text, size_t textlen, string_llist *destination)
 		//strcpy(newstring, text + stringbegin);
 		memcpy(newstring, text + stringbegin, stringsize);
 		newstring[stringsize] = '\0';
-			
+        
 		// push to linked list
 		string_llist_push_back(destination, newstring);
 		
@@ -75,6 +75,33 @@ void parse_all(parser *p, char *text, size_t textlen, string_llist *destination)
 		offset = stringend;
 	}
 }
+
+void clean_search_results(string_llist *list)
+{
+    char pattern[] = "((^(.(?!http))*$)|(<a onclick|<a class|google.com))";
+    parser *jargonParser = init_parser(pattern);
+    
+    //int vector[jargonParser->vectorsize];
+	int vector[jargonParser->vectorsize];
+    int offset = 0;
+	
+	string_node **node = &list->front;
+    while(*node != NULL)
+	{
+        string_node *current = *node;
+		// find matches for re with text, optimized w/ study
+		int retval = pcre_exec(jargonParser->re, NULL,//p->study,
+                               current->string, strlen(current->string), offset, 0,
+                               vector, jargonParser->vectorsize);
+		
+		node = &current->next;
+        
+        // if match found, delete from list
+		if (retval > 0)
+            string_llist_delete_node(list, &current);
+	}
+}
+
 
 void kill_parser(parser *p)
 {
--- utils/parser.h
+++ utils/parser.h
@@ -22,6 +22,11 @@ parser *init_parser(char *regex);
 void parse_all(parser *p, char *text, size_t textlen, string_llist *destination);
 
 /*
+ * cleans jargon links from search engine results page
+ */
+void clean_search_results(string_llist *list);
+
+/*
  * Free memory associated with a parser
  */
 void kill_parser(parser *p);
