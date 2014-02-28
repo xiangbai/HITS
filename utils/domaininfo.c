4821f760dee7da00c120190c82a70188edd15c7f
Tyler <tyler.c.hoffman@gmail.com>
2014-02-28 13:40:33 -0500

    Update domaininfo to index outlinks

--- utils/domaininfo.c
+++ utils/domaininfo.c
@@ -1,40 +1,184 @@
 #include "domaininfo.h"
+#include "binarytree.h"
+#include "parser.h"
 #include <stdlib.h>
 #include <string.h>
 
+#define DOMAIN_INDEX 	2
+#define NUM_SUBSTRINGS	3
+
+char domain_pattern[] = "(\\w+\\.)*(\\w+\\.\\w+)$";
+parser *domain_parser;
+
+void domain_regex_init()
+{
+	domain_parser = init_parser(domain_pattern);
+}
+
+/**
+ * Get the domain without subdomains.
+ * For example, if a domain of test.google.com is provided
+ *	google.com will be returned.
+ *
+ *	domain_name: domain to parse
+ *
+ * returns the domain without its subdomains.
+ */
+char *get_name_sans_subdomains(char *domain_name)
+{
+	int i;
+	// initialize array to store substrings
+	char *substrings[NUM_SUBSTRINGS];
+	for (i = 0; i < NUM_SUBSTRINGS; i++)
+		substrings[i] = NULL;
+	
+	// parse substrings into array
+	substrings_to_array(domain_parser, domain_name, strlen(domain_name), 0, substrings);
+
+	// free all strings except the part of the domain we want
+	for (i = 0; i < NUM_SUBSTRINGS; i++)
+		if (i != DOMAIN_INDEX)
+			free(substrings[i]);
+	
+	// return domain
+	return substrings[DOMAIN_INDEX];
+}
+
+/**
+ * Compare two domains.
+ *
+ *	a: First domain
+ *	b: Second somain
+ *
+ * Returns strcmp(a->name, b->name)
+ *
+ * Note: This is not used internally, but will have functionality in collections of domaininfos.
+ */
 int compare_domain_name(domaininfo *a, domaininfo *b)
 {
-	return strcmp(a->name, b->name);
+	// get domains without subdomains
+	char *domain_a = get_name_sans_subdomains(a->name);
+	char *domain_b = get_name_sans_subdomains(b->name);
+
+	// get comparison
+	int compare_val = strcmp(domain_a, domain_b);
+	
+	// free strings
+	free(domain_a);
+	free(domain_b);
+	
+	// return value
+	return compare_val;
+}
+
+/**
+ * Compare function for domaininfo and domainlink
+ * Used to compare domaininfos to links held in domaininfo.outlinks
+ */
+int compare_domain_to_link(domaininfo *domain, domainlink *link)
+{
+	// get domains without subdomains
+	char *domain_a = get_name_sans_subdomains(domain->name);
+	char *domain_b = get_name_sans_subdomains(link->domain->name);
+	
+	// get comparison
+	int compare_val = strcmp(domain_a, domain_b);
+	
+	// free strings
+	free(domain_a);
+	free(domain_b);
+	
+	// return value
+	return compare_val;
+}
+
+/**
+ * Compare function for domainlinks
+ * Used for domainlink.pages llist
+ */
+int compare_domainlink(domainlink* linka, domainlink* linkb)
+{
+	return strcmp(linka->domain->name, linkb->domain->name);
 }
 
 domaininfo *domaininfo_init(char *domainname)
 {
 	domaininfo *domain = (domaininfo *)malloc(sizeof(domaininfo));
-	domain->name = (char *)malloc(sizeof(char) * (strlen(domainname) + 1));
-	strcpy(domain->name, domainname);
-	domain->numpages = 0;
-	llist_init(&domain->pages, &compare_domain_name);
+	//domain->name = (char *)malloc(sizeof(char) * (strlen(domainname) + 1));
+	domain->name = get_name_sans_subdomains(domainname);
+	//domain->numpages = 0;
+	//llist_init(&domain->pages, &compare_domain_name);
+	btree_init(&domain->outlinks, (void*)&compare_domain_to_link);
 	return domain;
 }
 
-void domaininfo_pushurl(domaininfo *domain, urlinfo *url)
+void domaininfo_puturl(domaininfo *domain, urlinfo *url)
 {
-	llist_push_back(&domain->pages, url);
-	domain->numpages++;
+	//llist_push_back(&domain->pages, url);
+	//domain->numpages++;
+	domainlink* domain_found = (domainlink*)btree_find(&domain->outlinks, (void*)url);
+	if (domain_found)
+	{
+		// add new url to pages linked list
+		llist_push_back(&domain_found->pages, (void*)url);
+	}
+	else
+	{
+		// no appropriate domainlink found; create it
+		domainlink *newdomain = (domainlink*)malloc(sizeof(domainlink));
+		newdomain->domain = domaininfo_init(url->host);		// domain name
+		llist_init(&newdomain->pages, (int (*)(void*, void*))&compare_domainlink);	// llist for pages
+		
+		// add new url to pages linked list
+		llist_push_back(&newdomain->pages, (void*)url);
+		
+		// add newdomain to outlinks
+		btree_insert(&domain->outlinks, newdomain);
+	}
 }
 
-urlinfo *domaininfo_popurl(domaininfo *domain)
+int domaininfo_numlinks_to_domain(domaininfo *domain, domaininfo *link)
 {
-	urlinfo *output = llist_pop_front(domain);
-	if (output)
-		domain->numpages--;
-	return output;
+	domainlink* domain_found = (domainlink*)btree_find(&domain->outlinks, (void*)link);
+	if (domain_found)
+		return domain_found->pages.size;
+	else
+		return 0;
 }
 
 void freedomain(domaininfo *domain)
 {
-	// empty linked list
-	while (domain->numpages)
-		llist_pop_back(&domain->pages);
-	free(domain);
+	// iterate through domain->outlinks and remove internal data
+	treenode *node = domain->outlinks.root;
+	int size = domain->outlinks.numElems;
+	
+	// get the first node
+	if (node)
+	{
+		// get the left-most node
+		while (node->left)
+			node = node->left;
+	}
+	
+	// remove node's data and iterate through tree
+	size_t i;
+	for (i = 0; i < size; i++)
+	{
+		// free current node's data
+		domainlink *link = (domainlink*)node->data;
+		llist_free(&link->pages, 0);
+		
+		// get next node
+       	if (node->rightthread)
+       	    node = node->right;
+       	else
+       	{
+       	    node = node->right;
+       	    while(!node->leftthread)
+       	        node = node->left;
+       	}
+	}
+
+	// free the binary tree last
+	btree_free(&domain->outlinks, 0);
 }
--- utils/domaininfo.h
+++ utils/domaininfo.h
@@ -1,35 +1,91 @@
 #ifndef DOMAININFO_H
 #define DOMAININFO_H
 
-#include "llist.h"
-#include "stdlib.h"
+//#include "llist.h"
+#include "binarytree.h"
+#include "parser.h"
 #include "../urlinfo.h"
+#include <stdlib.h>
 
-/***************************************
+/************************************************
  * domaininfo.h
- * Holds info associated with each host
- *  and methods for using it
- **************************************/
+ * Holds links from one domain to other domains
+ *  and the urls linked to by the initial domain
+ * 
+ * Sample Structure:
+ * 
+ * domaininfo	domaininfo	   urlinfo
+ * ----------	----------	-------------------
+ * 						   *-- a.com/index.html
+ * 						  /
+ *			   *- a.com -*
+ * 			  /		 	  \
+ * 			 /			   *-- a.com/test.html
+ * d.com   -*
+ * 			 \			   *-- b.com/index.html
+ * 			  \     	  /
+ * 			   *- b.com -*
+ * 						  \
+ * 						   *-- b.com/hello.html
+ * 
+ ************************************************/
 
 typedef struct domaininfo
 {
 	char *name;
-	size_t numpages;
-	llist pages;
+	//llist pages;
+	btree outlinks;	// hold domainlinks
 } domaininfo;
 
+/* 
+ * Struct to hold links from one domain to another
+ * Kleinberg recommends no more than 4-8 links form one domain to another
+ * Note: domainlinks are not to hold all pages on a domain.
+ *	They are to hold links from a domaininfo to pages on the another given domain
+ *	Many domainlinks will reference the same domaininfo
+ * 	IE if a.com links to b.com/cs.html and d.com links b.com/e.html:
+ *		A domaininfo for a.com holds domainlink for b.com in its binary tree
+ *		That domainlink holds a reference to c.html in its llist.
+ *		The domaininfo for d.com holds a different domainlink for b.com
+ */
+typedef struct domainlink
+{
+	domaininfo *domain;
+	llist pages;		// holds pages in domain linked to from domaininfo
+} domainlink;
+
+/**
+ * Initialize regex for making domaininfo
+ * This must be called before initializing domaininfos.
+ */
 domaininfo *domaininfo_init(char *domain);
 
-void domaininfo_pushurl(domaininfo *domain, urlinfo *url);
+/**
+ * Add link to url from domaininfo.
+ * If domain.outlinks doesn't contain url's domain, it is added
+ *	domain:	domain to hold the link
+ *	url: url domain will link to
+ */
+void domaininfo_puturl(domaininfo *domain, urlinfo *url);
 
-urlinfo *domaininfo_popurl(domaininfo *domain);
+/**
+ * Get the number links from one domain to another
+ *	domain:	domain holding the links
+ *	link:	domain linked to by domain
+ */
+int domaininfo_numlinks_to_domain(domaininfo *domain, domaininfo *link);
 
 /*
+ * Compare 2 domains
  * Returns 1 if provided domains have the same name.
  * Returns 0 otherwise
  */
 int compare_domain_name(domaininfo *a, domaininfo *b);
 
+/**
+ * Free data held by domain
+ * This will not free the domain name or associated urlinfos
+ */
 void freedomain(domaininfo *domain);
 
 #endif
